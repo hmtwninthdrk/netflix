dd7a54f7e8541db77f16d0fa74159bbcc420ce90
whynur <dauletova.ainur2801@gmail.com>
2022-11-27 00:38:24 +0600

    feat:add new modal

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 import Intro from "./components/Intro"
+import Modal from "./components/Modal/Modal"
+import {useState} from "react";
+
 
 function App() {
+    const [modalActive,setModalActive]=useState(true)
     return (
         <>
-            <Header />
-            <Intro />
-            <Main />
-            <div>This is react project</div>
-            <div>This is Ainur!</div>
-            <Footer />
+            <Header/>
+            <Intro/>
+            <Main/>
+            <Footer/>
+            <Modal active={modalActive} setActive={setModalActive}/>
         </>
     )
 }
